2b8afcb92e34636b9139fb2c0f10f9ec4262f6ff
7wen <995776164@qq.com>
2021-04-07 11:20:17 +0800

    Eureka+小集群

--- springcloud/spring_cloud_provider_dept_8001/src/main/java/com/wen/springcloud/DeptProvider_8001.java
+++ springcloud/spring_cloud_provider_dept_8001/src/main/java/com/wen/springcloud/DeptProvider_8001.java
@@ -4,6 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author 7wen
@@ -12,6 +14,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @MapperScan("com.wen.springcloud.dao")
+@EnableEurekaClient //cs架构 将客户端注册到注册中心中
+@EnableDiscoveryClient //配置服务发现
 public class DeptProvider_8001 {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider_8001.class, args);
--- springcloud/spring_cloud_provider_dept_8001/src/main/java/com/wen/springcloud/controller/DeptController.java
+++ springcloud/spring_cloud_provider_dept_8001/src/main/java/com/wen/springcloud/controller/DeptController.java
@@ -1,9 +1,12 @@
 package com.wen.springcloud.controller;
 
+import com.netflix.discovery.converters.Auto;
 import com.wen.springcloud.pojo.Dept;
 import com.wen.springcloud.service.DeptService;
 import com.wen.springcloud.service.DeptServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +23,9 @@ import java.util.List;
 public class DeptController {
     @Autowired
     DeptServiceImpl deptService;
+    //获取配置信息,获取微服务清单
+    @Autowired
+    DiscoveryClient discoveryClient;
 
     @PostMapping("/dept/add")
     public boolean addDept(Dept dept) {
@@ -34,5 +40,20 @@ public class DeptController {
     @GetMapping("/dept/queryAll")
     public List<Dept> queryAll() {
         return deptService.queryAll();
+    }
+
+    //注册微服务 获取一些信息
+    @GetMapping("/dept/discovery")
+    public Object discovery() {
+        //获取微服务列表的清单
+        List<String> services = discoveryClient.getServices();
+        System.out.println("discovery:"+services);
+
+        //得到一个具体的微服务信息 通过服务的名称
+        List<ServiceInstance> instances = discoveryClient.getInstances("SPRINGCLOUD-PROVIDER-DEPT");
+        instances.forEach(i->{
+            System.out.println("host:"+i.getHost()+",uri"+i.getUri()+",port"+i.getPort());
+        });
+        return this.discoveryClient;
     }
 }
